a5fc1af70463efbafeb383ae77f816625e2fb290
IgorTecnologia <iguigu2019@gmail.com>
2024-04-08 15:44:05 -0300

    import.sql

---
+++ apiclients/src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_clients (name, cpf, income, birth_date, children) VALUES ('João Silva', '123.456.789.10', 2200.10, TIMESTAMP WITH TIME ZONE '1992-06-10T20:50:07.12345Z', 4);
+INSERT INTO tb_clients (name, cpf, income, birth_date, children) VALUES ('Maria Souza', '123.456.789.11', 2300.20, TIMESTAMP WITH TIME ZONE '1993-07-10T21:50:07.12345Z', 1);
+INSERT INTO tb_clients (name, cpf, income, birth_date, children) VALUES ('Mario Ferrira', '123.456.789.12', 2400.130, TIMESTAMP WITH TIME ZONE '1994-06-10T22:50:07.12345Z', 2);
+INSERT INTO tb_clients (name, cpf, income, birth_date, children) VALUES ('Carlos Andrade', '123.456.789.13', 2500.40, TIMESTAMP WITH TIME ZONE '1995-06-10T23:50:07.12345Z', 3);
+INSERT INTO tb_clients (name, cpf, income, birth_date, children) VALUES ('André Carlos', '123.456.789.15', 2600.50, TIMESTAMP WITH TIME ZONE '1996-06-10T00:50:07.12345Z', 2);
+INSERT INTO tb_clients (name, cpf, income, birth_date, children) VALUES ('Andressa Batista', '123.456.789.16', 2700.60, TIMESTAMP WITH TIME ZONE '1997-06-10T20:50:07.12345Z', 3);
+INSERT INTO tb_clients (name, cpf, income, birth_date, children) VALUES ('Luisa Correia', '123.456.789.17', 2800.70, TIMESTAMP WITH TIME ZONE '1998-06-10T01:50:07.12345Z', 5);
+INSERT INTO tb_clients (name, cpf, income, birth_date, children) VALUES ('Silva Maria', '123.456.789.18', 2900.80, TIMESTAMP WITH TIME ZONE '1999-06-10T02:50:07.12345Z', 2);
+INSERT INTO tb_clients (name, cpf, income, birth_date, children) VALUES ('Pedro Henrrique', '123.456.789.19', 3000.90, TIMESTAMP WITH TIME ZONE '2000-06-10T03:50:07.12345Z', 1);
+INSERT INTO tb_clients (name, cpf, income, birth_date, children) VALUES ('Jéssica Maisa', '123.456.789.10', 3100.19, TIMESTAMP WITH TIME ZONE '2001-06-10T04:50:07.12345Z', 1);
