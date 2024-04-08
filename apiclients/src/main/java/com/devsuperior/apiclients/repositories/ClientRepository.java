20576df4ff17619d86a6c3db311b1fefe205254c
IgorTecnologia <iguigu2019@gmail.com>
2024-04-08 15:14:47 -0300

    Client Repository

--- apiclients/src/main/java/com/devsuperior/apiclients/repositories/ClientRepository.java
+++ apiclients/src/main/java/com/devsuperior/apiclients/repositories/ClientRepository.java
@@ -1,4 +1,4 @@
-package com.devsuperior.apiclients.repository;
+package com.devsuperior.apiclients.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
