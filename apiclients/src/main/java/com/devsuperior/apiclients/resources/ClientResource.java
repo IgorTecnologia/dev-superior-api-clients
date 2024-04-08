dece6f2ca6c464e7d2bca1da5634db6815248f18
IgorTecnologia <iguigu2019@gmail.com>
2024-04-08 18:18:10 -0300

    Client Resource

--- apiclients/src/main/java/com/devsuperior/apiclients/resources/ClientResource.java
+++ apiclients/src/main/java/com/devsuperior/apiclients/resources/ClientResource.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -62,5 +63,10 @@ public class ClientResource {
 		return ResponseEntity.ok().body(dto);
 	}
 	
-	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<ClientDTO> delete(@PathVariable Long id) {
+		service.delete(id);
+		
+		return ResponseEntity.noContent().build();
+	}
 }
