44282b1eb64664190118bb104b91878404826221
IgorTecnologia <iguigu2019@gmail.com>
2024-04-08 18:18:58 -0300

    DataBaseException

--- apiclients/src/main/java/com/devsuperior/apiclients/services/exceptions/DataBaseException.java
+++ apiclients/src/main/java/com/devsuperior/apiclients/services/exceptions/DataBaseException.java
@@ -1,5 +1,10 @@
 package com.devsuperior.apiclients.services.exceptions;
 
-public class DataBaseException {
+public class DataBaseException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public DataBaseException(String msg) {
+		super(msg);
+	}
 
 }
