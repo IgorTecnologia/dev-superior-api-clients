3835c0f0a05b4f27de8b3ddc280f78e8a6c634ab
IgorTecnologia <iguigu2019@gmail.com>
2024-04-08 15:43:03 -0300

    Client Entity

--- apiclients/src/main/java/com/devsuperior/apiclients/entities/Client.java
+++ apiclients/src/main/java/com/devsuperior/apiclients/entities/Client.java
@@ -4,12 +4,17 @@ import java.time.Instant;
 import java.util.Objects;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_clients")
 public class Client {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String cpf;
