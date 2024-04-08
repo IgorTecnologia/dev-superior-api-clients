5799a6715fad67bc0bc7c989f2aca3b00cb40e91
IgorTecnologia <iguigu2019@gmail.com>
2024-04-08 17:36:09 -0300

    Client Resource

--- apiclients/src/main/java/com/devsuperior/apiclients/resources/ClientResource.java
+++ apiclients/src/main/java/com/devsuperior/apiclients/resources/ClientResource.java
@@ -1,15 +1,22 @@
 package com.devsuperior.apiclients.resources;
 
-import java.util.List;
+import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.devsuperior.apiclients.entities.Client;
+import com.devsuperior.apiclients.dto.ClientDTO;
 import com.devsuperior.apiclients.services.ClientService;
 
 @RestController
@@ -20,14 +27,32 @@ public class ClientResource {
 	@Autowired
 	private ClientService service;
 	@GetMapping
-	public ResponseEntity<List<Client>> findAll(){
-		List<Client> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+	public ResponseEntity<Page<ClientDTO>> findAll(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "6") Integer linesPerPage,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+			){
+	
+			PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+			Page<ClientDTO> list = service.findAllPaged(pageRequest);
+			
+			return ResponseEntity.ok().body(list);
 	}
 	
 	@GetMapping("/{id}")
-	public ResponseEntity<Client> findById(@PathVariable Long id){
-		Client entity = service.findById(id);
-		return ResponseEntity.ok().body(entity);
+	public ResponseEntity<ClientDTO> findById(@PathVariable Long id){
+		ClientDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
 	}
+	
+	@PostMapping
+	public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto){
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
+	
+	
 }
