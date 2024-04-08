6ccb810aff0dad8b2213de2142648025791bc267
IgorTecnologia <iguigu2019@gmail.com>
2024-04-08 14:54:43 -0300

    Backend project created

---
+++ apiclients/src/test/java/com/devsuperior/apiclients/ApiclientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.apiclients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiclientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
