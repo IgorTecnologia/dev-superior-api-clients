b05efe9d58906bb76a2bcf5e98242707511ac6de
IgorTecnologia <iguigu2019@gmail.com>
2024-04-08 16:32:56 -0300

    Client Service

--- apiclients/src/main/java/com/devsuperior/apiclients/services/ClientService.java
+++ apiclients/src/main/java/com/devsuperior/apiclients/services/ClientService.java
@@ -1,13 +1,16 @@
 package com.devsuperior.apiclients.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.ResourceAccessException;
 
 import com.devsuperior.apiclients.entities.Client;
 import com.devsuperior.apiclients.repositories.ClientRepository;
+import com.devsuperior.apiclients.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -20,4 +23,10 @@ public class ClientService {
 		return repository.findAll();
 	}
 	
+	@Transactional(readOnly = true)
+	public Client findById(Long id){
+		Optional <Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found " + id));
+		return entity;
+	}
 }
