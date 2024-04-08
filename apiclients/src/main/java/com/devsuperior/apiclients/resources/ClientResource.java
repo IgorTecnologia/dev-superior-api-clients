d683ca469eeaaf0d3b720ecd2f5d492fb0779dd8
IgorTecnologia <iguigu2019@gmail.com>
2024-04-08 15:26:36 -0300

    Client Resource

---
+++ apiclients/src/main/java/com/devsuperior/apiclients/resources/ClientResource.java
@@ -0,0 +1,26 @@
+package com.devsuperior.apiclients.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.apiclients.entities.Client;
+import com.devsuperior.apiclients.services.ClientService;
+
+@RestController
+@RequestMapping(value = "/Clients")
+public class ClientResource {
+
+	
+	@Autowired
+	private ClientService service;
+	@GetMapping
+	public ResponseEntity<List<Client>> findAll(){
+		List<Client> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+}
